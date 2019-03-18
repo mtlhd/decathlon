2b0312d6aa7ee8dd582a6ec25c522208391d8fcd
Henrik Söderqvist <henrik.m.soderqvist@gmail.com>
2019-03-18 18:43:46 +0100

    Update decathlon.js

--- decathlon.js
+++ decathlon.js
@@ -2,14 +2,14 @@
 var parameters = {};
 
 parameters['100m'] = {a: 25.4347, b: 18, c: 1.81};
-parameters['LongJump'] = {a: 0.14354, b: 220, c: 1.4};
-parameters['ShotPut'] = {a: 51.39, b: 1.5, c: 1.05};
-parameters['HighJump'] = {a: 0.8465, b: 75, c: 1.42};
+parameters['longjump'] = {a: 0.14354, b: 220, c: 1.4};
+parameters['shotput'] = {a: 51.39, b: 1.5, c: 1.05};
+parameters['highjump'] = {a: 0.8465, b: 75, c: 1.42};
 parameters['400m'] = {a: 1.53775, b: 82, c: 1.81};
-parameters['Hurdles'] = {a: 5.74352, b: 28.5, c: 1.92};
-parameters['Discus'] = {a: 12.91, b: 4, c: 1.1};
-parameters['Pole'] = {a: 0.2797, b: 100, c: 1.35};
-parameters['Javelin'] = {a: 10.14, b: 7, c: 1.08};
+parameters['hurdles'] = {a: 5.74352, b: 28.5, c: 1.92};
+parameters['discus'] = {a: 12.91, b: 4, c: 1.1};
+parameters['pole'] = {a: 0.2797, b: 100, c: 1.35};
+parameters['javelin'] = {a: 10.14, b: 7, c: 1.08};
 parameters['1500m'] = {a: 0.03768, b: 480, c: 1.85};
 
 function calcTrackPoints(result, athlete, event) {
@@ -31,3 +31,13 @@ function calcPoints(result, athlete, event) {
   var pointsField = document.getElementById(event + "-points-" + athlete);
   pointsField.value = points;
 }
+
+function calcTotal(athlete) {
+  var points = 0;
+  var keys = parameters.keys();
+  
+  for (index = 0; index < keys.length; ++index) {
+    var pointsField = document.getElementById(keys[index] + "-points-" + athlete);
+    points += pointsField.value;
+}
+  
